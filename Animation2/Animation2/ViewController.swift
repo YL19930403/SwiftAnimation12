8a4a90ef95b25ff912a5fb2ce4a296928bfb0fde
zhangli <313890735@qq.com>
2016-03-01 09:53:55 +0800

    搭建界面;2C

---
+++ Animation2/Animation2/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Animation2
+//
+//  Created by 余亮 on 16/3/1.
+//  Copyright © 2016年 余亮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var LoginLabel: UILabel!
+    
+    @IBOutlet weak var AccountF: UITextField!
+    
+    @IBOutlet weak var PasswordF: UITextField!
+    
+    @IBOutlet weak var LoginBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+
+ 
+
+}
+
